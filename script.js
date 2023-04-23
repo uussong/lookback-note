d536fbef067dc7015a77df96cdae3f0ad69f5c0a
uussong <77879633+uussong@users.noreply.github.com>
2023-04-23 18:57:07 +0900

    ✨ 새 메모 작성 시 메모 요소 생성

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let memoList = []
-const add = document.querySelector('.add')
+let memoList = JSON.parse(localStorage.getItem('memoList'))
+memoList = memoList ?? []
 
+
+const add = document.querySelector('.btn-add')
+const memoInput = document.querySelector('.memo-input')
+const submit = document.querySelector('btn-submit')
 
 const addMemo = () => {
   const learn = document.getElementById('learn').value
@@ -8,5 +12,39 @@ const addMemo = () => {
   localStorage.setItem('memoList', JSON.stringify(memoList))
 }
 add.addEventListener('click', addMemo)
+// add.addEventListener('click', () => {
+//   memoInput.classList.remove('hidden')
 
-const renderMemo = () => {}
+//   if (!memoInput.classList.contains('hidden')) {
+    
+//   }
+// })
+
+
+const renderMemo = () => {
+  const display = document.querySelector('.display')
+  const article = document.createElement('article')
+  const div = document.createElement('div')
+  const title = document.createElement('h2')
+  display.appendChild(article)
+  article.appendChild(div)
+  div.appendChild(title)
+  
+  for (const item of memoList) {
+    const content = document.createElement('p')
+    article.classList.add('display-list')
+    div.classList.add('display-item')
+    title.classList.add('display-title')
+    title.textContent = '배운점'
+    
+    content.textContent = item.learn
+    
+    div.appendChild(content)
+    console.log(item)
+  }
+
+}
+
+renderMemo()
+submit.addEventListener('click', addMemo)
+submit.addEventListener('click', renderMemo)
